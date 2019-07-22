1862a3ec68f21616125506dab565e9093d6911c2
daniel Woodford <daniel.woodford.g@gmail.com>
2019-07-22 21:27:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,15 +19,34 @@ end
 
 
 
+def reduce_to_total(source_array, starting_point=0)
+  i = 0
+  total = 0
+  while i < source_array.length do
+    total += source_array[i]
+    i += 1
+  end
+  return total + starting_point
+end
+
+
+
 def reduce_to_total(source_array, starting_point = 0)
  total = source_array.reduce(:+)
    return  starting_point + total
 end 
 
 def reduce_to_all_true(source_array)
-  source_array.all?
+  source_array.reduce{|i| !!i}
 end 
 
-def reduce_to_any_true(source_array)
-  source_array.any?
-end
+
+def reduce_to_any_true(source_array, starting_point = 0)
+  i = 0
+  total= starting_point
+  while i < source_array.length do
+    total = yield(total, source_array[i])
+    i += 1
+  end
+  return total 
+end 
